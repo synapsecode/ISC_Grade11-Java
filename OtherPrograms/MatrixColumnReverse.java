05054560868f78c4a3447d13aaade7876eb11b72
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2021-01-16 00:24:18 +0530

    RecordPrograms5

--- OtherPrograms/MatrixColumnReverse.java
+++ OtherPrograms/MatrixColumnReverse.java
@@ -1,43 +1,39 @@
-/*
-PROGRAM-15
-Transpose A Matrix
-*/
 package OtherPrograms;
+
 import java.util.Scanner;
-public class MatrixTranspose {
+
+public class MatrixColumnReverse {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Rows: ");
 		int r = sc.nextInt();
 		System.out.print("Enter Columns: ");
 		int c = sc.nextInt();
-		int[][] mat = new int[r][c];
-		int[][] transmat = new int[c][r];
-
-		System.out.println("Enter Elements:");
+		System.out.println();
+		int mat[][] = new int[r][c];
+		int revmat[][] = new int[r][c];
 		//Accepting
+		System.out.println("Enter Elements: ");
 		for(int i=0; i<r; i++){
 			for(int j=0; j<c; j++){
+				//Adding Data
 				mat[i][j] = sc.nextInt();
-				transmat[j][i] = mat[i][j];	
+				revmat[c-i-1][j] = mat[i][j];
 			}
 		}
-		//Printing
 		System.out.println("\nOriginal Matrix: ");
 		for(int i=0; i<r; i++){
 			for(int j=0; j<c; j++){
-				System.out.print(mat[i][j]);
+				System.out.print(mat[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println("\nTransposed Matrix");
-		//Printing
-		for(int i=0; i<c; i++){
-			for(int j=0; j<r; j++){
-				System.out.print(transmat[i][j]);
+		System.out.println("\nColumn Reversed Matrix: ");
+		for(int i=0; i<r; i++){
+			for(int j=0; j<c; j++){
+				System.out.print(revmat[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();
 	}
 }
---
+++ OtherPrograms/SymmetricMatrix.java
@@ -0,0 +1,42 @@
+//Program-19
+package OtherPrograms;
+
+import java.util.Scanner;
+
+public class SymmetricMatrix {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter N: ");
+		int n = sc.nextInt();
+		System.out.println();
+		int mat[][] = new int[n][n];
+		int transmat[][] = new int[n][n];
+		// Accepting
+		boolean is_symmetric = true;
+		System.out.println("Enter Elements: ");
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				// Adding Data
+				mat[i][j] = sc.nextInt();
+				transmat[j][i] = mat[i][j];
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < n; i++) {
+			if (is_symmetric) {
+				for (int j = 0; j < n; j++) {
+					if (mat[i][j] != transmat[i][j]) {
+						is_symmetric = false;
+						break;
+					}
+				}
+			} else
+				break;
+		}
+
+		if (is_symmetric)
+			System.out.println("Symmetric Matrix");
+		else
+			System.out.println("Unsymmetric Matrix");
+	}
+}
